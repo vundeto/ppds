166dba6f6b05a31ca832a52df86d1025a758ed29
vundeto <mitko.vandev@abv.bg>
2024-06-11 21:21:42 +0300

    equijoin fixed

---
+++ ppds_api_java/src/org/ppds/operators/GroupBy.java
@@ -0,0 +1,22 @@
+package org.ppds.operators;
+
+import org.ppds.core.ONCIterator;
+import org.ppds.core.Record;
+
+public class GroupBy implements ONCIterator {
+
+    @Override
+    public void open() {
+
+    }
+
+    @Override
+    public Record next() {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
